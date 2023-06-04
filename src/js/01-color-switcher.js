f258ae8cc7f7d56c8302d34daa4aa629dc308e47
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-06-04 11:50:23 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const timer = {
+    intervalId: null,
+    isActive: false,
+    
+    start() {
+        if (this.isActive) {
+            return;
+        }
+
+        this.isActive = true;
+        this.intervalId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        this.toggleButtons();
+    },
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        
+        this.toggleButtons();
+    },
+
+    toggleButtons() {
+        btnStart.disabled = this.isActive;
+        btnStop.disabled = !this.isActive
+    },
+};
+
+btnStart.addEventListener('click', () => timer.start());
+btnStop.addEventListener('click', () => timer.stop());
